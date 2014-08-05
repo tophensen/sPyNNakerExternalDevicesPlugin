72fd84c49d00610b0a08b2d4161f96b9db530303
alan-stokes <alan.barry.stokes@gmail.com>
2014-08-05 14:26:20 +0100

    moved virutal vertex from pynnaker to spinnmachine

--- spynnaker_with_external_devices/pyNN/external_devices_models/abstract_external_device.py
+++ spynnaker_with_external_devices/pyNN/external_devices_models/abstract_external_device.py
@@ -1,7 +1,16 @@
-from spynnaker.pyNN.models.abstract_models.abstract_virtual_vertex \
+#pacman imports
+from pacman.model.constraints.\
+    vertex_requires_virtual_chip_in_machine_constraint import \
+    VertexRequiresVirtualChipInMachineConstraint
+
+#spinnmachine imports
+from spinn_machine.abstract_virtual_vertex \
     import AbstractVirtualVertex
+
+#inhittance imports
 from abc import ABCMeta
 from six import add_metaclass
+
 
 @add_metaclass(ABCMeta)
 class ExternalDevice(AbstractVirtualVertex):
@@ -10,6 +19,10 @@ class ExternalDevice(AbstractVirtualVertex):
         AbstractVirtualVertex.__init__(
             self, n_neurons, virtual_chip_coords, connected_node_coords,
             connected_node_edge, label, max_atoms_per_core)
+        vertex_virtual_chip_constraint = \
+            VertexRequiresVirtualChipInMachineConstraint(
+                virtual_chip_coords, connected_node_coords, connected_node_edge)
+        self.add_constraint(vertex_virtual_chip_constraint)
 
     @property
     def model_name(self):
