776da8e685b36f220d428e7ae0d780eace77f92b
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-01-13 11:43:12 +0000

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="sPyNNakerExternalDevicesPlugin",
+    version="2015.001",
+    description="Spinnaker External Devices Plugin",
+    url="https://github.com/SpiNNakerManchester/SpyNNaker",
+    packages=['spynnaker_external_devices_plugin',
+              'spynnaker_external_devices_plugin.pyNN',
+              'spynnaker_external_devices_plugin.pyNN.abstract_models',
+              'spynnaker_external_devices_plugin.pyNN.control_models',
+              'spynnaker_external_devices_plugin.pyNN.external_devices_models',
+              'spynnaker_external_devices_plugin.pyNN.interfaces',
+              'spynnaker_external_devices_plugin.pyNN.links'],
+    install_requires=['sPyNNaker >= 2015.001']
+)
