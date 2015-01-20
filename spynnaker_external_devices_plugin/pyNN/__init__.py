93aad7f2dac75b02ae93a3ef72227c32ffb42d9a
alan-stokes <alan.barry.stokes@gmail.com>
2015-01-20 14:38:45 +0000

    removed live packet gather from the spynnaker (basic pynn) and added it to the external device plugin.

--- spynnaker_external_devices_plugin/pyNN/__init__.py
+++ spynnaker_external_devices_plugin/pyNN/__init__.py
@@ -5,6 +5,7 @@ and implementation for the PyNN High-level API
 """
 
 #external models
+from spinnman.messages.eieio.eieio_type_param import EIEIOTypeParam
 from spynnaker_external_devices_plugin.pyNN.external_devices_models.\
     external_cochlea_device import ExternalCochleaDevice
 from spynnaker_external_devices_plugin.pyNN.external_devices_models.\
@@ -15,7 +16,36 @@ from spynnaker_external_devices_plugin.pyNN.external_devices_models.\
     munich_retina_device import MunichRetinaDevice
 from spynnaker_external_devices_plugin.pyNN.control_models.\
     munich_motor_control import MunichMotorControl
-from spynnaker_external_devices_plugin.pyNN.control_models.reverse_ip_tag_multi_cast_source \
-    import ReverseIpTagMultiCastSource
+from spynnaker_external_devices_plugin.pyNN.control_models.\
+    reverse_ip_tag_multi_cast_source import ReverseIpTagMultiCastSource
+from spynnaker_external_devices_plugin.pyNN.control_models.live_packet_gather \
+    import LivePacketGather
+from spynnaker_external_devices_plugin.pyNN.\
+    spynnaker_external_device_plugin_manager import \
+    SpynnakerExternalDevicePluginManager
+from spynnaker.pyNN.utilities import conf
 
-from spinnman.messages.eieio.eieio_prefix_type import EIEIOPrefixType
+spynnaker_external_devices = SpynnakerExternalDevicePluginManager()
+
+
+def activate_live_output_for(
+        population, port=None, host=None, tag=None,
+        strip_sdp=True, use_prefix=False, key_prefix=None,
+        prefix_type=None, message_type=EIEIOTypeParam.KEY_32_BIT,
+        right_shift=0, payload_as_time_stamps=True,
+        use_payload_prefix=True, payload_prefix=None,
+        payload_right_shift=0, number_of_packets_sent_per_time_step=0):
+    #get default params if none set
+    if port is None:
+        port = conf.config.getint("Recording", "live_spike_port")
+    if host is None:
+        host = conf.config.get("Recording", "live_spike_host")
+    if tag is None:
+        tag = conf.config.getint("Recording", "live_spike_tag")
+    #add new edge and vertex if required to spinnaker graph
+    spynnaker_external_devices.add_edge_to_recorder_vertex(
+        population._vertex, port, host, tag, strip_sdp, use_prefix,
+        key_prefix, prefix_type, message_type, right_shift,
+        payload_as_time_stamps, use_payload_prefix, payload_prefix,
+        payload_right_shift, number_of_packets_sent_per_time_step)
+
---
+++ spynnaker_external_devices_plugin/pyNN/spynnaker_external_device_plugin_manager.py
@@ -0,0 +1,38 @@
+from pacman.model.partitionable_graph.partitionable_edge import \
+    PartitionableEdge
+from spinnman.messages.eieio.eieio_type_param import EIEIOTypeParam
+from spynnaker_external_devices_plugin.pyNN import LivePacketGather
+from spynnaker.pyNN import get_spynnaker
+
+class SpynnakerExternalDevicePluginManager(object):
+
+    def __init__(self):
+        self._live_spike_recorders = dict()
+
+    def add_edge_to_recorder_vertex(
+            self, vertex_to_record_from, port, hostname, tag=None,
+            strip_sdp=True, use_prefix=False, key_prefix=None,
+            prefix_type=None, message_type=EIEIOTypeParam.KEY_32_BIT,
+            right_shift=0, payload_as_time_stamps=True,
+            use_payload_prefix=True, payload_prefix=None,
+            payload_right_shift=0, number_of_packets_sent_per_time_step=0):
+
+        _spinnaker = get_spynnaker()
+        # locate the live spike recorder
+        if (port, hostname) in self._live_spike_recorders:
+            live_spike_recorder = self._live_spike_recorders[(port, hostname)]
+        else:
+
+            live_spike_recorder = LivePacketGather(
+                _spinnaker.machine_time_step, _spinnaker.timescale_factor,
+                tag, port, hostname, strip_sdp, use_prefix, key_prefix,
+                prefix_type, message_type, right_shift, payload_as_time_stamps,
+                use_payload_prefix, payload_prefix, payload_right_shift,
+                number_of_packets_sent_per_time_step)
+            self._live_spike_recorders[(port, hostname)] = live_spike_recorder
+            _spinnaker.add_vertex(live_spike_recorder)
+
+        # create the edge and add
+        edge = PartitionableEdge(vertex_to_record_from,
+                                 live_spike_recorder, "recorder_edge")
+        _spinnaker.add_edge(edge)
