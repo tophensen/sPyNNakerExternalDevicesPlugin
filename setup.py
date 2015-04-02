64b33afb532b042694aedc377cedeff1a91581c4
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-04-02 16:13:41 +0100

    Updating for spynnaker version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sPyNNakerExternalDevicesPlugin",
-    version="2015.008-rc-01",
+    version="2015.008-rc-02",
     description="Spinnaker External Devices Plugin",
     url="https://github.com/SpiNNakerManchester/SpyNNaker",
     packages=['spynnaker_external_devices_plugin',
@@ -13,5 +13,5 @@ setup(
               'spynnaker_external_devices_plugin.pyNN.model_binaries',
               'spynnaker_external_devices_plugin.pyNN.utility_models'],
     package_data={'spynnaker_external_devices_plugin.pyNN.model_binaries': ['*.aplx']},
-    install_requires=['sPyNNaker >= 2015.004-rc-02']
+    install_requires=['sPyNNaker >= 2015.004-rc-03']
 )
