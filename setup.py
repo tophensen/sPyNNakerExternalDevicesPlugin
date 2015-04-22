a8d9a361850044cea506b50fb7f60d19a316b95a
Andrew Rowley <Andrew.Rowley@manchester.ac.uk>
2015-04-22 09:17:52 +0100

    Removed unnecessary dependency from install

--- setup.py
+++ setup.py
@@ -13,6 +13,5 @@ setup(
               'spynnaker_external_devices_plugin.pyNN.model_binaries',
               'spynnaker_external_devices_plugin.pyNN.utility_models'],
     package_data={'spynnaker_external_devices_plugin.pyNN.model_binaries': ['*.aplx']},
-    install_requires=['sPyNNaker >= 2015.004-rc-03',
-                      'SpiNNMan >= 2015.003-rc-02']
+    install_requires=['sPyNNaker >= 2015.004-rc-03']
 )
