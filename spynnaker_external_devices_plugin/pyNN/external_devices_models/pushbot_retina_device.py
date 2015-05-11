d2402c922aee8fbc1129ba33d7b579a11e288ceb
jamie <neworderofjamie@gmail.com>
2015-05-11 10:48:58 +0100

    Still very hacky, but functioning retina

--- spynnaker_external_devices_plugin/pyNN/external_devices_models/pushbot_retina_device.py
+++ spynnaker_external_devices_plugin/pyNN/external_devices_models/pushbot_retina_device.py
@@ -17,16 +17,16 @@ from spynnaker.pyNN.utilities.multi_cast_command import MultiCastCommand
 
 
 # robot with 7 7 1
-def get_x_from_robot_retina(key):
-    return (key >> 7) & 0x7f
+#def get_x_from_robot_retina(key):
+#    return (key >> 7) & 0x7f
 
 
-def get_y_from_robot_retina(key):
-    return key & 0x7f
+#def get_y_from_robot_retina(key):
+#    return key & 0x7f
 
 
-def get_spike_value_from_robot_retina(key):
-    return (key >> 14) & 0x1
+#def get_spike_value_from_robot_retina(key):
+#    return (key >> 14) & 0x1
 
 
 class PushBotRetinaDevice(AbstractVirtualVertex,
@@ -34,16 +34,19 @@ class PushBotRetinaDevice(AbstractVirtualVertex,
                          AbstractOutgoingEdgeSameContiguousKeysRestrictor):
 
     # key codes for the robot retina
-    MANAGEMENT_BIT = 0x80000000
-    MANAGEMENT_MASK = 0x7FFFF000
+    MANAGEMENT_MASK = 0xFFFFF800
     RETINA_ENABLE = 0x1
     RETINA_DISABLE = 0x0
     RETINA_KEY_SET = 0x2
-
-    POLARITY_BIT = 0x1000
+    RETINA_RESET = 0x7
     
+    SENSOR = 0x7F0
+    SENSOR_SET_KEY = 0x0
+    SENSOR_SET_PUSHBOT = 0x1
+
     UP_POLARITY = "UP"
     DOWN_POLARITY = "DOWN"
+    MERGED_POLARITY = "MERGED"
 
     def __init__(self, virtual_chip_x, virtual_chip_y,
                  connected_to_real_chip_x, connected_to_real_chip_y,
@@ -51,20 +54,22 @@ class PushBotRetinaDevice(AbstractVirtualVertex,
                  timescale_factor, spikes_per_second, ring_buffer_sigma,
                  label=None, n_neurons=None, polarity=None):
 
-        if polarity is None:
-            polarity = PushBotRetinaDevice.UP_POLARITY
+        # Default to up polarity
+        #if polarity is None:
+        #    polarity = PushBotRetinaDevice.UP_POLARITY
 
-        self._fixed_key = (virtual_chip_x << 24 | virtual_chip_y << 16)
-        fixed_n_neurons = 128 * 128
+        self._fixed_key = 0xFEFFF800#(virtual_chip_x << 24 | virtual_chip_y << 16)
+        fixed_n_neurons = 32 * 32 * 2 # MERGED
         
-        self._fixed_mask = 0xFFFFF000
+        # Lower 13 bits for retina payload
+        self._fixed_mask = 0xFFFFF800
         
-        if polarity == PushBotRetinaDevice.UP_POLARITY:
-            self._fixed_key |= PushBotRetinaDevice.POLARITY_BIT
-        elif polarity == PushBotRetinaDevice.DOWN_POLARITY:
-           pass
-        else:
-            assert False
+        #if polarity == PushBotRetinaDevice.UP_POLARITY:
+        #    self._fixed_key |= PushBotRetinaDevice.POLARITY_BIT
+        #elif polarity == PushBotRetinaDevice.DOWN_POLARITY:
+        #   pass
+        #else:FFFFF000
+        #    assert False
 
         AbstractVirtualVertex.__init__(
             self, fixed_n_neurons, virtual_chip_x, virtual_chip_y,
@@ -95,31 +100,49 @@ class PushBotRetinaDevice(AbstractVirtualVertex,
         """
         commands = list()
 
-        # change the retina key it transmits with
-        # (based off if its right or left)
-        key_set_command = self.MANAGEMENT_BIT | self.RETINA_KEY_SET
-
+        # reset retina
+        #reset_command = self.RETINA_RESET
+        
+        #commands.append(MultiCastCommand(
+        #    0, reset_command, self.MANAGEMENT_MASK, 0,
+        #    0, 0))
+        
         # to ensure populations receive the correct packets, this needs to be
         # different based on which retina
-        key_set_payload = (self._virtual_chip_x << 24 |
-                           self._virtual_chip_y << 16)
+        key_set_payload = 0xFEFFF800#(self._virtual_chip_x << 24 |
+                           #self._virtual_chip_y << 16)
 
+        # Set sensor key
         commands.append(MultiCastCommand(
-            0, key_set_command, self.MANAGEMENT_MASK, key_set_payload,
-            5, 1000))
-
-        # make retina enabled (dependant on if its a left or right retina
-        enable_command = self.MANAGEMENT_BIT | self.RETINA_ENABLE
+            0, self.SENSOR | self.SENSOR_SET_KEY, self.MANAGEMENT_MASK, key_set_payload,
+            0, 0))
+        
+        # Set pushbot
+        commands.append(MultiCastCommand(
+            0, self.SENSOR | self.SENSOR_SET_PUSHBOT, self.MANAGEMENT_MASK, 1,
+            0, 0))
+        
+        # Disable retina
+        commands.append(MultiCastCommand(
+            0, self.RETINA_DISABLE, self.MANAGEMENT_MASK, 0, 
+            0, 0))
+        
+        # Set retina key
+        commands.append(MultiCastCommand(
+            0, self.RETINA_KEY_SET, self.MANAGEMENT_MASK, key_set_payload,
+            0, 0))
+        
+        # Enable retina
+        enable_command = self.RETINA_ENABLE
         commands.append(MultiCastCommand(
             0, enable_command, self.MANAGEMENT_MASK,
-            (4 << 29) | (2 << 26),  # **YUCK** mystery flags
-            5, 1000))
+            (0 << 29) + (3 << 26),  # **YUCK** mystery flags
+            0, 0))
 
         # disable retina
-        disable_command = self.MANAGEMENT_BIT | self.RETINA_DISABLE
         commands.append(MultiCastCommand(
-            -1, disable_command, self.MANAGEMENT_MASK, 0, 
-            5, 1000))
+            -1, self.RETINA_DISABLE, self.MANAGEMENT_MASK, 0, 
+            0, 0))
 
         return commands
 
