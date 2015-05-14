d1e4a0fef6ba9a6ccb7f359f0619761119175ac7
james.knight <james.knight-5@postgrad.manchester.ac.uk>
2015-05-14 15:57:53 +0100

    Hopefully fully functioning pushbot retina

--- spynnaker_external_devices_plugin/pyNN/__init__.py
+++ spynnaker_external_devices_plugin/pyNN/__init__.py
@@ -13,7 +13,7 @@ from spynnaker_external_devices_plugin.pyNN.external_devices_models.\
 from spynnaker_external_devices_plugin.pyNN.external_devices_models.\
     munich_retina_device import MunichRetinaDevice
 from spynnaker_external_devices_plugin.pyNN.external_devices_models.\
-    pushbot_retina_device import PushBotRetinaDevice
+    pushbot_retina_device import PushBotRetinaDevice, PushBotRetinaResolution, PushBotRetinaPolarity
 from spynnaker_external_devices_plugin.pyNN.utility_models.spike_injector \
     import SpikeInjector
 from spynnaker_external_devices_plugin.pyNN import model_binaries
--- spynnaker_external_devices_plugin/pyNN/external_devices_models/pushbot_retina_device.py
+++ spynnaker_external_devices_plugin/pyNN/external_devices_models/pushbot_retina_device.py
@@ -12,65 +12,88 @@ from spinn_front_end_common.abstract_models\
     .abstract_outgoing_edge_same_contiguous_keys_restrictor\
     import AbstractOutgoingEdgeSameContiguousKeysRestrictor
 
+from collections import namedtuple
+from enum import Enum, IntEnum
 from pacman.model.routing_info.key_and_mask import KeyAndMask
 from spynnaker.pyNN.utilities.multi_cast_command import MultiCastCommand
 
+# Named tuple bundling together configuration elements of a pushbot resolution config
+PushBotRetinaResolutionConfig = namedtuple("PushBotRetinaResolution", 
+                                           ["pixels", "enable_command", "coordinate_bits"])
 
-# robot with 7 7 1
-#def get_x_from_robot_retina(key):
-#    return (key >> 7) & 0x7f
-
-
-#def get_y_from_robot_retina(key):
-#    return key & 0x7f
-
-
-#def get_spike_value_from_robot_retina(key):
-#    return (key >> 14) & 0x1
-
+PushBotRetinaResolution = Enum(
+    value="PushBotRetinaResolution",
+    names=[("Native128",    PushBotRetinaResolutionConfig(128, (1 << 26), 7)),
+           ("Downsample64", PushBotRetinaResolutionConfig(64, (2 << 26), 6)),
+           ("Downsample32", PushBotRetinaResolutionConfig(32, (3 << 26), 5)),
+           ("Downsample16", PushBotRetinaResolutionConfig(16, (4 << 26), 4))])
+    
+PushBotRetinaPolarity = IntEnum(
+    value="PushBotRetinaPolarity",
+    names=["Up", "Down", "Merged"])
 
 class PushBotRetinaDevice(AbstractVirtualVertex,
                          AbstractSendMeMulticastCommandsVertex,
                          AbstractOutgoingEdgeSameContiguousKeysRestrictor):
 
-    # key codes for the robot retina
+    # Mask for all SpiNNaker->Pushbot commands
     MANAGEMENT_MASK = 0xFFFFF800
+    
+    # Retina-specific commands
     RETINA_ENABLE = 0x1
     RETINA_DISABLE = 0x0
     RETINA_KEY_SET = 0x2
-    RETINA_RESET = 0x7
+    RETINA_NO_TIMESTAMP = (0 << 29)
     
+    # Sensor commands
     SENSOR = 0x7F0
     SENSOR_SET_KEY = 0x0
     SENSOR_SET_PUSHBOT = 0x1
-
-    UP_POLARITY = "UP"
-    DOWN_POLARITY = "DOWN"
-    MERGED_POLARITY = "MERGED"
 
     def __init__(self, virtual_chip_x, virtual_chip_y,
                  connected_to_real_chip_x, connected_to_real_chip_y,
                  connected_to_real_chip_link_id, machine_time_step,
                  timescale_factor, spikes_per_second, ring_buffer_sigma,
-                 label=None, n_neurons=None, polarity=None):
-
-        # Default to up polarity
-        #if polarity is None:
-        #    polarity = PushBotRetinaDevice.UP_POLARITY
-
-        self._fixed_key = 0xFEFFF800#(virtual_chip_x << 24 | virtual_chip_y << 16)
-        fixed_n_neurons = 32 * 32 * 2 # MERGED
+                 label=None, n_neurons=None, 
+                 polarity=PushBotRetinaPolarity.Merged,
+                 resolution=PushBotRetinaResolution.Downsample64):
         
-        # Lower 13 bits for retina payload
-        self._fixed_mask = 0xFFFFF800
+        # Validate number of timestamp bytes
+        if not isinstance(polarity, PushBotRetinaPolarity):
+            raise exceptions.ConfigurationException("Pushbot retina polarity should be one of those defined in Polarity enumeration")
+        if not isinstance(resolution, PushBotRetinaResolution):
+            raise exceptions.ConfigurationException("Pushbot retina resolution should be one of those defined in Resolution enumeration")
         
-        #if polarity == PushBotRetinaDevice.UP_POLARITY:
-        #    self._fixed_key |= PushBotRetinaDevice.POLARITY_BIT
-        #elif polarity == PushBotRetinaDevice.DOWN_POLARITY:
-        #   pass
-        #else:FFFFF000
-        #    assert False
-
+        # Cache resolution
+        self._resolution = resolution
+        
+        # Build standard routing key from virtual chip coordinates
+        self._routing_key = virtual_chip_x << 24 | virtual_chip_y << 16
+        self._retina_source_key = self._routing_key
+        
+        # Calculate number of neurons
+        fixed_n_neurons = resolution.value.pixels ** 2
+        
+        # If polarity is merged
+        if polarity == PushBotRetinaPolarity.Merged:
+            # Double number of neurons
+            fixed_n_neurons *= 2
+           
+            # We need to mask out two coordinates and a polarity bit
+            mask_bits = (2 * resolution.value.coordinate_bits) + 1
+        # Otherwise
+        else:
+            # We need to mask out two coordinates
+            mask_bits = 2 * resolution.value.coordinate_bits
+            
+            # If polarity is up, set polarity bit in routing key
+            if polarity == PushBotRetinaPolarity.Up:
+                polarity_bit = 1 << (2 * resolution.value.coordinate_bits)
+                self._routing_key |= polarity_bit
+        
+        # Build routing mask
+        self._routing_mask = ~((1 << mask_bits) - 1) & 0xFFFFFFFF
+        
         AbstractVirtualVertex.__init__(
             self, fixed_n_neurons, virtual_chip_x, virtual_chip_y,
             connected_to_real_chip_x, connected_to_real_chip_y,
@@ -79,8 +102,6 @@ class PushBotRetinaDevice(AbstractVirtualVertex,
         AbstractSendMeMulticastCommandsVertex.__init__(
             self, self._get_commands())
         AbstractOutgoingEdgeSameContiguousKeysRestrictor.__init__(self)
-
-        self._polarity = polarity
  
         if n_neurons != fixed_n_neurons and n_neurons is not None:
             print "Warning, the retina will have {} neurons".format(
@@ -91,65 +112,56 @@ class PushBotRetinaDevice(AbstractVirtualVertex,
                        .get_outgoing_edge_constraints(
                            self, partitioned_edge, graph_mapper))
         constraints.append(KeyAllocatorFixedKeyAndMaskConstraint(
-            [KeyAndMask(self._fixed_key, self._fixed_mask)]))
+            [KeyAndMask(self._routing_key, self._routing_mask)]))
         return constraints
 
     def _get_commands(self):
         """
         method that returns the commands for the retina external device
         """
-        commands = list()
-
-        # reset retina
-        #reset_command = self.RETINA_RESET
-        
-        #commands.append(MultiCastCommand(
-        #    0, reset_command, self.MANAGEMENT_MASK, 0,
-        #    0, 0))
-        
-        # to ensure populations receive the correct packets, this needs to be
-        # different based on which retina
-        key_set_payload = 0xFEFFF800#(self._virtual_chip_x << 24 |
-                           #self._virtual_chip_y << 16)
-
         # Set sensor key
+        commands = list()
         commands.append(MultiCastCommand(
-            0, self.SENSOR | self.SENSOR_SET_KEY, self.MANAGEMENT_MASK, key_set_payload,
+            0, PushBotRetinaDevice.SENSOR | PushBotRetinaDevice.SENSOR_SET_KEY, 
+            PushBotRetinaDevice.MANAGEMENT_MASK, self._retina_source_key,
             0, 0))
         
-        # Set pushbot
+        # Set sensor to pushbot
         commands.append(MultiCastCommand(
-            0, self.SENSOR | self.SENSOR_SET_PUSHBOT, self.MANAGEMENT_MASK, 1,
+            0, PushBotRetinaDevice.SENSOR | PushBotRetinaDevice.SENSOR_SET_PUSHBOT, 
+            PushBotRetinaDevice.MANAGEMENT_MASK, 1,
             0, 0))
         
-        # Disable retina
+        # Ensure retina is disabled
         commands.append(MultiCastCommand(
-            0, self.RETINA_DISABLE, self.MANAGEMENT_MASK, 0, 
+            0, PushBotRetinaDevice.RETINA_DISABLE, 
+            PushBotRetinaDevice.MANAGEMENT_MASK, 0, 
             0, 0))
         
         # Set retina key
         commands.append(MultiCastCommand(
-            0, self.RETINA_KEY_SET, self.MANAGEMENT_MASK, key_set_payload,
+            0, PushBotRetinaDevice.RETINA_KEY_SET, 
+            PushBotRetinaDevice.MANAGEMENT_MASK, self._retina_source_key,
             0, 0))
         
         # Enable retina
-        enable_command = self.RETINA_ENABLE
         commands.append(MultiCastCommand(
-            0, enable_command, self.MANAGEMENT_MASK,
-            (0 << 29) + (3 << 26),  # **YUCK** mystery flags
+            0, PushBotRetinaDevice.RETINA_ENABLE, 
+            PushBotRetinaDevice.MANAGEMENT_MASK,
+            PushBotRetinaDevice.RETINA_NO_TIMESTAMP + self._resolution.value.enable_command,
             0, 0))
 
-        # disable retina
+        # At end of simulation, disable retina
         commands.append(MultiCastCommand(
-            -1, self.RETINA_DISABLE, self.MANAGEMENT_MASK, 0, 
+            -1, PushBotRetinaDevice.RETINA_DISABLE, 
+            PushBotRetinaDevice.MANAGEMENT_MASK, 0, 
             0, 0))
 
         return commands
 
     @property
     def model_name(self):
-        return "external retina device at " \
-               "with _polarity {}".format(self._polarity)
+        return "pushbot retina device"
 
     def recieves_multicast_commands(self):
         return True
