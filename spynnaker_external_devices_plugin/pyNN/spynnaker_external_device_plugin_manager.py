a6bad5906f380051eee229535e67578bc8cb8b51
alan-stokes <alan.barry.stokes@gmail.com>
2015-07-06 16:27:40 +0100

    removal of database params commit

--- spynnaker_external_devices_plugin/pyNN/spynnaker_external_device_plugin_manager.py
+++ spynnaker_external_devices_plugin/pyNN/spynnaker_external_device_plugin_manager.py
@@ -15,6 +15,17 @@ class SpynnakerExternalDevicePluginManager(object):
     def __init__(self):
         self._live_spike_recorders = dict()
 
+    def add_socket_address(self, socket_address):
+        """
+        helper method for adding a socket address to the list needed to be
+        checked by the notifcation protocol
+        :param socket_address: the socket address
+        :type socket_address:
+        :return:
+        """
+        _spinnaker = get_spynnaker()
+        _spinnaker._add_socket_address(socket_address)
+
     def add_edge_to_recorder_vertex(
             self, vertex_to_record_from, port, hostname, tag=None,
             board_address=None,
@@ -51,7 +62,66 @@ class SpynnakerExternalDevicePluginManager(object):
             connected_to_real_chip_y, connected_to_real_chip_link_id, speed=30,
             sample_time=4096, update_time=512, delay_time=5,
             delta_threshold=23, continue_if_not_different=True,
-            model=IF_curr_exp, params={}):
+            model=IF_curr_exp, params=None):
+        """ Create a population of 6 neurons which will drive the robot motor.
+        Neuron id 0 drives the forwards direction
+                  1 drives the backwards direction
+                  2 drives the left direction
+                  3 drives the right direction
+                  4 drives the clockwise direction
+                  5 drives the counter-clockwise direction
+        The control works by counting the number of spikes sent to each\
+        neuron, and then taking the difference between opposite\
+        directions at every sample time timesteps.  If the difference is\
+        above the threshold, a command is sent to move the robot in the\
+        appropriate direction.
+        The robot currently needs to be assigned a "virtual chip", which\
+        is then used to determine the routing keys that will be used for\
+        the motor control.  This chip must be one which does not exist\
+        within the current physical machine.
+
+        :param virtual_chip_x: The x coordinate of the virtual chip
+        :type virtual_chip_x: int
+        :param virtual_chip_y: The y coordinate of the virtual chip
+        :type virtual_chip_y: int
+        :param connected_to_real_chip_x: The x coordinate of the real chip in\
+                    the machine where the spinn-link is connected
+        :type connected_to_real_chip_x: int
+        :param connected_to_real_chip_y: The y coordinate of the real chip in\
+                    the machine where the spinn-link is connected
+        :type connected_to_real_chip_x: int
+        :param connected_to_real_chip_link_id: The id of the link on the real\
+                    chip where the spinn-link is connected
+        :type connected_to_real_chip_link_id: int
+        :param speed: The speed to be sent to the motor when a direction is\
+                    activated
+        :type speed: int
+        :param sample_time: The number of time steps between samples of the\
+                    spike counts to determine the motor direction
+        :type sample_time: int
+        :param update_time: The number of time steps between sending updates\
+                    of the motor speed
+        :type update_time: int
+        :param delay_time: The minimum number of microseconds between commands\
+                    sent to the robot
+        :type delay_time: int
+        :param delta_threshold: The threshold of the difference between spike\
+                    counts of opposite directions over which a direction will\
+                    be chosen
+        :type delta_threshold: int
+        :param continue_if_not_different: Determines if the robot should\
+                    continue in the current direction until a new direction\
+                    is chosen, or if it should only move for one period\
+                    before stopping unless the same direction is chosen\
+                    again
+        :type continue_if_not_different: bool
+        :param model: The neuron model to use for the pre-motor population
+        :type model: class
+        :param params: Dictionary of parameters to give to the model
+        :type params: dict or None
+        :return: The newly created population
+        :rtype: Population
+        """
         spynnaker = get_spynnaker()
         population = Population(6, model, params, spynnaker,
                                 "Robot Input Population")
--- spynnaker_external_devices_plugin/pyNN/utility_models/spike_injector.py
+++ spynnaker_external_devices_plugin/pyNN/utility_models/spike_injector.py
@@ -14,6 +14,7 @@ class SpikeInjector(ReverseIpTagMultiCastSource,
     def __init__(self, n_neurons, machine_time_step, timescale_factor,
                  spikes_per_second, ring_buffer_sigma, label, port,
                  virtual_key=None):
+
         ReverseIpTagMultiCastSource.__init__(
             self, port=port, label=label,
             n_neurons=n_neurons, machine_time_step=machine_time_step,
